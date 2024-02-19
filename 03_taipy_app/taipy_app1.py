a6e8392aa09d1136dc26b8ef0a92f5f9fe605cb3
daspallab772 <40576319+daspallab772@users.noreply.github.com>
2024-02-19 09:10:14 +0530

    tyteyy

--- 03_taipy_app/taipy_app1.py
+++ 03_taipy_app/taipy_app1.py
@@ -1,26 +1,2 @@
 import taipy as tp
 
-# Previous configuration of scenario
-...
-
-page = """
-Name: <|{input_name}|input|>
-<|submit|button|on_action=submit_scenario|>
-
-Message: <|{message}|text|>
-"""
-
-input_name = "Taipy"
-message = None
-
-
-def submit_scenario(state):
-    state.scenario.input_name.write(state.input_name)
-    state.scenario.submit(wait=True)
-    state.message = scenario.message.read()
-
-
-if __name__ == "__main__":
-    tp.Core().run()
-    scenario = tp.create_scenario(scenario_cfg)
-    tp.Gui(page).run()
